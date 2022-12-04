8aaf814421516347beb8b643ce4ad50f585ca88a
julien <julien.drouart.pro@gmail.com>
2022-12-04 21:49:55 +0100

    add 2022 j2 part2

--- 2022/2/2.php
+++ 2022/2/2.php
@@ -3,18 +3,6 @@
 $data = explode(PHP_EOL,file_get_contents("data.txt"));
 $score = 0;
 
-/*
-A Rock
-B Paper
-C Scissors
-
-X Rock
-Y Paper
-Z Scissors
-
-1 for Rock, 2 for Paper, and 3 for Scissors
-*/
-
 foreach($data as $k => $v)
 {
     $lineData = explode(" ",$v);
@@ -22,13 +10,11 @@ foreach($data as $k => $v)
     {
         if($lineData[1] == "X") //Rock
         {
-            $score += 3;
-            $score += 1;
+            $score += 4;
         }
         if($lineData[1] == "Y") //Paper
         {
-            $score += 6;
-            $score += 2;
+            $score += 8;
         }
         if($lineData[1] == "Z") //Scissors
         {
@@ -43,21 +29,18 @@ foreach($data as $k => $v)
         }
         if($lineData[1] == "Y") //Paper
         {
-            $score += 3;
-            $score += 2;
+            $score += 5;
         }
         if($lineData[1] == "Z") //Scissors
         {
-            $score += 6;
-            $score += 3;
+            $score += 9;
         }
     }
     if($lineData[0] == "C") //Scissors
     {
         if($lineData[1] == "X") //Rock
         {
-            $score += 6;
-            $score += 1;
+            $score += 7;
         }
         if($lineData[1] == "Y") //Paper
         {
@@ -65,18 +48,72 @@ foreach($data as $k => $v)
         }
         if($lineData[1] == "Z") //Scissors
         {
-            $score += 3;
-            $score += 3;
+            $score += 6;
         }
     }
-    echo "<pre>";
-    print_r($lineData);echo "<br>";
+
+}
+echo "<pre>";
 print_r($score);
 echo "<pre>";
+
+/*
+X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+1 for Rock, 2 for Paper, and 3 for Scissors
+0 if you lost, 3 if the round was a draw, and 6 if you won
+*/
+$score = 0;
+foreach($data as $k => $v)
+{
+    $lineData = explode(" ",$v);
+    if($lineData[0] == "A") //Rock
+    {
+        if($lineData[1] == "X") //lose
+        {
+            $score += 3;
+        }
+        if($lineData[1] == "Y") //draw
+        {
+            $score += 4;
+        }
+        if($lineData[1] == "Z") //win
+        {
+            $score += 8;
+        }
+    }
+    if($lineData[0] == "B") //Paper
+    {
+        if($lineData[1] == "X") //lose
+        {
+            $score += 1;
+        }
+        if($lineData[1] == "Y") //draw
+        {
+            $score += 5;
+        }
+        if($lineData[1] == "Z") //win
+        {
+            $score += 9;
+        }
+    }
+    if($lineData[0] == "C") //Scissors
+    {
+        if($lineData[1] == "X") //lose
+        {
+            $score += 2;
+        }
+        if($lineData[1] == "Y") //draw
+        {
+            $score += 6;
+        }
+        if($lineData[1] == "Z") //win
+        {
+            $score += 7;
+        }
+    }
 }
 
-
 echo "<pre>";
-print_r("");
+print_r($score);
 echo "<pre>";
 
