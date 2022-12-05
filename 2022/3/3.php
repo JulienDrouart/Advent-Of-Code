e9876b70b290f7262be0deaa74cdb132b98e4a0d
julien <julien.drouart.pro@gmail.com>
2022-12-05 22:17:46 +0100

    add 2022 d3 p2

--- 2022/3/3.php
+++ 2022/3/3.php
@@ -1,12 +1,6 @@
 <?php
 
 $data = explode(PHP_EOL,file_get_contents("data.txt"));
-/*$data = explode(PHP_EOL,"vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw");*/
 $prioritiesSum = 0;
 foreach($data as $k => $v)
 {
@@ -17,6 +11,26 @@ foreach($data as $k => $v)
     $prioritiesSum += array_search($itemType,$alphabet);
 }
 
+$prioritiesSum = 0;
+$i = 0;
+$array = array();
+$arrayKey = 0;
+foreach($data as $k => $v)
+{
+    $array[$arrayKey][] = $v; 
+    if($i== 2)
+    {
+        
+        $itemType = array_values(array_intersect(str_split($array[$arrayKey][0]),str_split($array[$arrayKey][1]),str_split($array[$arrayKey][2])))[0]; //find the duplicates in both part
+        $alphabet = array_merge(range('a', 'z'),range('A', 'Z')); //create the alphabet array
+        $alphabet = array_combine(range(1, count($alphabet)), array_values($alphabet)); //increase all index by 1
+        $prioritiesSum += array_search($itemType,$alphabet);
+        $arrayKey++;
+        $i=0;
+        continue;
+    }
+    $i++;
+}
 echo "<pre>";
 print_r($prioritiesSum);
 echo "<pre>";
